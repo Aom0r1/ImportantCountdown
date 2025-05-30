936785c489068296a3f71af1a0782c9f9ac9dde6
Aomori <vladislavzorin2006@gmail.com>
2025-05-30 15:25:29 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Count of important things: ";
+    std::cin >> n;
+
+    for (int i = n; i >= 0; i--) {
+        std::cout << "Important things remain: " << i << "!" << std::endl;
+    }
+
+    std::cout << "All important things are done!" << std::endl;
+    return 0;
+}

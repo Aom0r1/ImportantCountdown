c062fd03aae44b53004c897dcd5f97d39188b72d
Aomori <59709559+Aom0r1@users.noreply.github.com>
2025-05-30 16:10:04 +0300

    Update main.cpp
    upd 1.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
+#include <windows.h>
 
 int main() {
     int n;
     std::cout << "Count of important things: ";
     std::cin >> n;
+    std::cin.ignore();
 
-    for (int i = n; i >= 0; i--) {
-        std::cout << "Important things remain: " << i << "!" << std::endl;
+    for (int i = n; i > 0; i--) {
+        std::cout << "Important things remain: " << i << std::endl;
+        std::cin.get();
+        Beep(50, 3000);
     }
 
     std::cout << "All important things are done!" << std::endl;
